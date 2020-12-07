43b88818f0f07b3eb6e10bab005bd8d3cd7164c2
Jessica Palomo <jesicalynpalomo@gmail.com>
2020-12-07 01:26:35 -0500

    changes to Articles Form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Article from "./components/articles/Article";
+import Articles from "./components/articles/Articles";
+import ArticlesForm from "./components/articles/ArticlesForm";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Container>
+          <Switch>
+            <Route path="/articles/new" component={ArticlesForm} />
+            <Route path="/articles/:articleId/edit" component={ArticlesForm} />
+            <Route path="/articles/:articleId" component={Article} />
+            <Route path="/articles" component={Articles} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
